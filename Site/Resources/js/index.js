be56a7df14882cfbf056fec7218ad6fffb935b56
Juan Douglas lima da Silva <juandouglas2004@gmail.com>
2022-05-21 01:51:03 -0300

    Animação contato

--- Site/Resources/js/index.js
+++ Site/Resources/js/index.js
@@ -1,3 +1,34 @@
+var actualCarousel = 0;
+
+$(function () {
+    var contact = $('.contact .right');
+    var showing = false;
+    var area1 = contact.offset().top - $(window).height() + (contact.height() / 2);
+    var list = [];
+
+    $('p span').each(function (i, obj) {
+        list.push(obj);
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > area1) {
+            contact.addClass('show');
+            showing = true;
+        } else if (showing) {
+            contact.removeClass('show');
+        }
+    });
+});
+
 function more() {
+
+}
+
+function nextCarousel() {
+
+}
+
+function animateSpan(span) {
+
 
 }
